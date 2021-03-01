7ab2f0f5f047bf4a8707478a7a9ca005bcb17891
Kaimana Kahalekai <kkahalekai@eagles.ewu.edu>
2021-02-28 19:58:31 -0800

    Trying to fix the memory leak at line 20, added freeing each row

--- StringRearrangement_test.c
+++ StringRearrangement_test.c
@@ -5,35 +5,38 @@
 void sortString(char *s[], int count);
 void printArray(char *s[], int count);
 void swapStrings(char** a, char** b);
-int main(){
+int main() {
     int i;
-        char buff[BUFSIZ];
-        int count = 0;
-       // 's' is a pointer to a char pointer, initially 's' is allocated storage for one char pointer
-    char** s= malloc(sizeof (char *));
+    char buff[BUFSIZ];
+    int count = 0;
+    // 's' is a pointer to a char pointer, initially 's' is allocated storage for one char pointer
+    char **s = malloc(1 * sizeof(char *));
 
-        printf("Here is the list of unsorted names: \n\n");
+    printf("Here is the list of unsorted names: \n\n");
 
-        // Keep on reading unlimited number of names until EOF (Ctrl + D) is reached
-        for (count = 0; fgets(buff, sizeof(buff), stdin); count++){
-            //Step 1: allocate sufficient storage for s[count] to store the content in buff plus one byte for '\0'
-            s[count] =  malloc(strlen(buff)+1);
-            // Step 2: Copy into the allocated memory
-            strcpy(s[count], buff);
+    // Keep on reading unlimited number of names until EOF (Ctrl + D) is reached
+    for (count = 0; fgets(buff, sizeof(buff), stdin); count++) {
+        //Step 1: allocate sufficient storage for s[count] to store the content in buff plus one byte for '\0'
+        s[count] = malloc(strlen(buff) + 1);
+        // Step 2: Copy into the allocated memory
+        strcpy(s[count], buff);
 
-            //Step 3: resize the array of pointers pointed to by 's' to increase its size for the next pointer
-            s = realloc(s, sizeof (char*)*(count+2));
-//            As a debug for the program to actually run without EOF
-            if (count == 4){
-                break;
-            }
-            }
-        printf("\nCount is %d\n\n", count);
-       sortString(s, count);
-       printArray(s, count);
-       free(s);
+        //Step 3: resize the array of pointers pointed to by 's' to increase its size for the next pointer
+        s = realloc(s, sizeof(char *) * (count + 2));
+//      As a debug for the program to actually run without EOF
+        if (count == 4) {
+            break;
+        }
+    }
+    printf("\nCount is %d\n\n", count);
+    sortString(s, count);
+    printArray(s, count);
+    for ( i = 0; i < count; i++){
+        free(s[i]);
+    }
+    free(s);
 
-        return 0;
+    return 0;
 }
 
 void sortString(char *s[], int count){
